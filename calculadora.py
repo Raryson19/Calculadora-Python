606311bc5afcf6daaab6b37989052b8210ff55af
WolfDev <48846312+Raryson19@users.noreply.github.com>
2019-03-25 15:07:06 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,16 @@
+a=float(input("digite um numero:")
+b=float(input("digite um numero:")
+c=float(input("digite um numero:")
+print()
+if (b=='*'):
+    resultado=(a*c)
+    print (resultado)
+    if (b=='/'):
+    resultado=(a/c)
+    print (resultado)
+    if (b=='+'):
+    resultado=(a+c)
+    print (resultado)
+    if (b=='-'):
+    resultado=(a-c)
+    print (resultado)
